9fd3d1a3e19ce014da8801ae765264b79ff2465d
Ridhima Sharma <sharmaridhima0028@gmail.com>
2026-06-10 17:00:52 +0530

    fix mongoose connection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await  mongoose.connect('mongodb+srv://fooduser:Food1234@cluster0.sxrax6m.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-
+    await mongoose.connect(process.env.MONGODB_URI, {
+        serverSelectionTimeoutMS: 10000,
+        socketTimeoutMS: 45000,
+    }).then(() => console.log("DB Connected"));
 }
